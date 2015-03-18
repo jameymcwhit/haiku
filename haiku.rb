a0af7bf2aa8b4c5c382273c4e6989c3290e9f676
Jamey <jameymcwhit@gmail.com>
2015-03-18 11:32:21 -0400

    fixed it

--- haiku.rb
+++ haiku.rb
@@ -5,7 +5,7 @@
 
 puts("Sunny Day")
 
-puts("It is too bright outside.")
+puts("It's too bright outside.")
 puts("I'm far too sleepy for this light.")
 puts("Best go back to bed.")
 puts("Haiku's don't have four lines.")
