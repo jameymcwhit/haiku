d9372ed9fba1aee2d623db1af04fe39d8658b4cc
Jamey <jameymcwhit@gmail.com>
2015-03-18 11:06:39 -0400

    removed and changed stuff

--- haiku.rb
+++ haiku.rb
@@ -1,13 +1,10 @@
 # Here is a comment.
 # This too is a comment.
 
-# Trying something new.
 
 puts("Sunny Day")
 
-
-# New comment.
-puts("It's too bright outside.")
+puts("It is too bright outside.")
 puts("I'm far too sleepy for this light.")
 puts("Best go back to bed.")
 puts("Haiku's don't have four lines.")
